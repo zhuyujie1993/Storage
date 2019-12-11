467d641c310dcee487f1ace2c2b65aecbff23b4c
zhuyujie1993 <1123508251@qq.com>
2019-12-11 21:55:10 +0800

    代理服务器构建

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://192.168.0.108:8022/',
+                // target: 'http://192.168.0.108:8022/',
+                target: 'http://ff.suiyongrongxin.com/',
                 // secure: true,  // 如果是https接口，需要配置这个参数
                 changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
                 ws: true,
